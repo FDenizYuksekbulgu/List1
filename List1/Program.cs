ceeeac2bad8697c9fb44c1b303dfd31215713e6c
Deniz Yuksekbukgu <fdenizyuksekbulgu@gmail.com>
2025-01-15 12:47:46 +0300

    Add project files.

---
+++ List1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        List<string> davetliler = new List<string> 
+        { "Bülent Ersoy", "Ajda Pekkan", "Ebru Gündeş", "Hadise", "Hande Yener", "Tarkan", "Funda Arar", "Demet Akalın" };
+        //Davetli listesi oluşturuldu.
+
+        Console.WriteLine("Davetli Listesi:"); //Davetlileri foreach kullanarak tek tek yazdırıyoruz.
+        foreach (string davetli in davetliler)
+        { 
+            Console.WriteLine(davetli);
+        }
+
+
+    }
+}
+
